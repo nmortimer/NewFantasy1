db0c2f47c3f6ac119039f22bae5985c7c0872cbb
nmortimer <31416644+nmortimer@users.noreply.github.com>
2025-08-26 22:35:39 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,7 @@ module.exports = {
     "./app/**/*.{ts,tsx,js,jsx}"
   ],
   theme: {
-    extend: {
-      boxShadow: { soft: "0 10px 30px rgba(0,0,0,.35)" }
-    }
+    extend: { boxShadow: { soft: "0 10px 30px rgba(0,0,0,.35)" } }
   },
   plugins: []
 };
